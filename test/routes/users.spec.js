4c535d8871c105a20b4ca3ce424d88b4c3dd3aa5
gabrielezagarella <gabry-zaga20@hotmail.it>
2020-05-30 17:46:22 +0200

    added post into test index

--- test/routes/users.spec.js
+++ test/routes/users.spec.js
@@ -2,5 +2,24 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = chai.expect;
 const app = require('../../app');
+const {expectJson, createUser} = require('./utils/index');
 
 chai.use(chaiHttp);
+
+describe('[index] Get: /users', () => {
+    
+    it('-password', async () => {
+        const result = await chai.request(app).get ('/users');
+        expect(result.status).to.be.equal(200);
+        expect(result.body).to.be.instanceOf(Array);
+        expect(result.body).to.has.lengthOf(0);
+        expectJson(result);
+    })
+    // let createdUser = undefined;
+    // before('create user', async () => {
+    //   createdUser = await createUser();
+    // });
+    // after('delete user', () => {
+    //   createdUser ? createdUser.remove() : console.log('missing user');
+    // });
+})
