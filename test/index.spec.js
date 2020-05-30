0f856386aefa5be7a3d6e257ee359a0a7d8ee89f
Simone Aiello <35459772+dandelionsam@users.noreply.github.com>
2020-05-30 16:58:53 +0200

    Create test for GET/index route (#3)
    Co-authored-by: dandelionsam <aiellosimone@gmail.com>

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,20 @@
-const expect = require('mocha');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const expect = chai.expect;
+const app = require('../app');
 
 chai.use(chaiHttp);
+
+describe('GET /', (
+
+  it('should be born to be alive 🕺💿', async() => {
+
+    const result = await chai.request(app).get('/');
+
+    expect(result.status).to.be.equal(200);
+    expect(result.body).to.have.property('message');
+    expect(result.body.message).to.be.equal('I\'m alive');
+
+  })
+
+));
