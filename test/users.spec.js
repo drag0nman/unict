25496e5483efb4941d31d48e336852e76437ae68
Giusy Giangravè <49571332+GlitchXCIV@users.noreply.github.com>
2020-05-30 17:32:35 +0200

    test users (#5)
    * test users
    
    * adding some checks

--- test/users.spec.js
+++ test/users.spec.js
@@ -1,5 +1,20 @@
-const expect = require('mocha');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const expect = chai.expect;
+const app = require('../app');
 
 chai.use(chaiHttp);
+
+describe('GET /', (
+
+  it('test users', async() => {   
+    const result = await chai.request(app).get('/users');
+    expect(result.status).to.be.equal(200);
+    expect(result.body).to.be.instanceOf(Array);
+    expect(result.body).to.has.lengthOf(0);
+  })
+
+  
+
+));
+
