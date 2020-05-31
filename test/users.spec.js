569432a04bcc77978713afdac1c553b584bc4f4c
Giusy Giangravè <49571332+GlitchXCIV@users.noreply.github.com>
2020-05-31 14:57:23 +0200

    Created test delete: /users/:id

--- test/users.spec.js
+++ test/users.spec.js
@@ -13,8 +13,32 @@ describe('GET /', (
     expect(result.body).to.be.instanceOf(Array);
     expect(result.body).to.has.lengthOf(0);
   })
-
-  
-
 ));
 
+describe('DELETE: /users/:id', () => {
+    it('delete user by id', async () => {
+      const newObjectId = mongoose.Types.ObjectId();
+      const result = await chai.request(app)
+          .delete(`/users/${newObjectId}`);
+      expect(result).to.have.property('status', 404);
+      expect(result).to.have.property('body');
+      expect(result.body).to.be.deep.equals(expectedNotFoundError);
+    });
+    describe('With an existing user', () => {
+        let createdUser = undefined;
+        beforeEach('create user', async () => {
+          createdUser = await createUser();
+        });
+        afterEach('delete user', () => {
+          createdUser ? createdUser.remove() : console.log('missing user');
+        });
+        it('Delete existing user', async () => {
+          const result = await chai.request(app)
+              .delete(`/users/${createdUser._id.toString()}`);
+          expect(result).to.have.property('status', 200);
+          expect(result).to.have.property('body');
+          expect(result.body).to.be.deep.equals({message: 'User deleted'})
+        });
+      })
+
+});
